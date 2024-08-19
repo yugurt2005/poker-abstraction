7e5701f764ebfa3dbb649513124877a37a7a742a
Chien-Yu Xiong <58307327+yugurt2005@users.noreply.github.com>
2024-08-18 21:39:53 -0700

    action tree

--- src/main.rs
+++ src/main.rs
@@ -10,29 +10,33 @@ pub fn main() {
 
     let path: String = "data/histograms/".to_string();
 
-    let strength = Rc::new(get_strengths(
+    let strength = Rc::new(tables::get_strengths(
         path.clone() + "strength.bin",
         &evaluator,
     ));
 
     let file: String = "data/tables/".to_string();
 
-    let _flop = get_flop_clusters(
+    let _flop = tables::get_flop_clusters(
+        2197,
         file.clone() + "flop.bin",
         path.clone() + "flop.bin",
         &strength,
     );
-    let _turn = get_turn_clusters(
+    let _turn = tables::get_turn_clusters(
+        2197,
         file.clone() + "turn.bin",
         path.clone() + "turn.bin",
         &strength,
     );
-    let ochs = get_ochs_clusters(
+    let ochs = tables::get_ochs_clusters(
+        13,
         file.clone() + "ochs.bin",
         path.clone() + "ochs.bin",
         &strength,
     );
-    let _river = get_river_clusters(
+    let _river = tables::get_river_clusters(
+        2197,
         file.clone() + "river.bin",
         path.clone() + "river.bin",
         Rc::clone(&evaluator),
